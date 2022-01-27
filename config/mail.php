45768686c3731353c68083a88cb6ce5d6e679e40
Stefan Ledin <stefan.ledin@gmail.com>
2022-01-27 18:02:26 +0100

    Guzzle

--- config/mail.php
+++ config/mail.php
@@ -50,7 +50,12 @@ return [
         ],
 
         'mailgun' => [
-            'transport' => 'mailgun',
+            'transport' => 'smtp',
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.eu.mailgun.net'),
         ],
 
         'postmark' => [
